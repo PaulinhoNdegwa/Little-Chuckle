983022b8f93ccc8deb2ec9ec230a32230be07b4e
Paul Gichuki <pherndegz@gmail.com>
2021-08-13 15:24:22 +0300

    Fix jokes card UI

--- src/components/FilterForm/index.js
+++ src/components/FilterForm/index.js
@@ -31,7 +31,7 @@ class FilterJokes extends Component {
         }
 
         return (
-            <form autoComplete="off" className={`${hideFilters ? `hidden` : ``} flex flex-col bg-indigo-100 px-4 py-3 my-2 max-w-7xl mx-auto sm:flex-row sm:flex-wrap sm:justify-evenly`}>
+            <form autoComplete="off" className={`${hideFilters ? `hidden` : ``} flex flex-col px-2 bg-gray-200 py-3 my-2 max-w-7xl mx-1 sm:mx-auto sm:flex-row sm:flex-wrap sm:justify-evenly`}>
                 {/* <div className="my-1">
                     <label className="ml-1 mt-1 text-sm font-normal" htmlFor="word">Enter Search Words</label>
                     <input autoComplete="off" onChange={handleSearchWordChange} value={word} className="block h-8 w-full sm:w-full md:w-2/3 lg:w-60 py-1 border-b-1 border-gray-500 text-sm rounded-lg focus:border-lg focus:border-indigo-500" type="text" name="word" id="word" placeholder="Search for a joke" />
--- src/components/Joke/index.js
+++ src/components/Joke/index.js
@@ -6,37 +6,34 @@ const Joke = props => {
         const values = Object.values(flags);
         return keys.map((flag, index) => {
             return (
-                values[index] ? <span key={index} className="italic font-light">{flag}, </span> : ""
+                values[index] ? <span key={index} className="italic font-medium text-sm py-1 px-1.5 bg-gray-300 text-black rounded-md mx-1">{flag}</span> : ""
             )
 
         })
     }
     return (
-        <div className={` ${selectedIndex === index ? `bg-indigo-100 border-gray-400 transform transition hover:-rotate-6` : ``}py-4 px-3 m-2 sm:m-1 sm:w-64 md:w-80 border-2 border-gray-200 rounded-sm py-2 px-4 hover:bg-gray-100 hover:border-gray-400`}>
+        <div className={`border-2 ${joke.safe ? `border-gray-900` : `border-red-400 transform transition hover:-rotate-2`} hover:bg-gray-100 hover:border-gray-400 py-4 px-3 m-2 sm:m-1 sm:w-64 md:w-80 rounded-sm py-2 px-4`}>
+            <p className="my-2 font-semibold text-indigo-500">{joke.category}</p>
+
             {
                 joke.type === "twopart" ? (
                     <div>
-                        <p className="my-2 font-mono antialiased font-bold">{joke.setup}</p>
-                        <p className="my-2 font-mono antialiased font-bold mt-7">{joke.delivery}</p>
+                        <p className="my-2 font-mono antialiased font-bold break-words ">{joke.setup}</p>
+                        <p className="my-2 font-mono antialiased font-bold mt-7 break-words ">{joke.delivery}</p>
 
                     </div>
                 ) : (
-                    <p className="my-2 font-mono antialiased font-bold">{joke.joke}</p>
+                    <p className="my-2 font-mono antialiased font-bold break-words ">{joke.joke}</p>
                 )
             }
-            <p className="my-2 font-semibold text-indigo-500">Category: {joke.category}           {joke.type === "twopart" ? "- Two-Part" : null}</p>
-            {
-                (Object.keys(joke.flags).length) > 0 ? (
-                    <p className="my-2 font-medium text-gray-500">Flags: {mapFlags(joke.flags)}</p>
-                ) : null
-            }
+            <p className="my-3 font-medium text-gray-500">{mapFlags(joke.flags)}</p>
 
             {
                 joke.safe
                     ?
-                    <p className="py-1/2 rounded-sm text-sm text-center font-semibold px-2 border-2 border-green-400 bg-green-300 w-16">Safe</p>
+                    <p className="py-1/2 mt-2 rounded-md text-sm text-center font-semibold px-2 border-2 border-green-400 bg-green-300 w-16">Safe</p>
                     :
-                    <p className="py-1/2 rounded-sm text-sm text-center font-semibold px-2 border-2 border-red-400 bg-red-300 w-16">NSFW</p>
+                    <p className="py-1/2 mt-2 rounded-md text-sm text-center font-semibold px-2 border-2 border-red-400 bg-red-300 w-16">NSFW</p>
             }
             {/* {selectedIndex == index ? <span className="text-gray-800 pt-4 bg-red-200">Joke of the day</span> : null} */}
         </div>

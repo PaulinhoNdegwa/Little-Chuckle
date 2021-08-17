de38f5e44b3888ae2b0040fa1eadd41a17cbf376
Paul Gichuki <pherndegz@gmail.com>
2021-08-17 07:59:11 +0300

    Fix bug on sticky header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,9 @@
 const Header = () => {
     return (
-        <div className="sticky top-0 bg-white max-w-8xl w-full sm:mx-auto py-5 px-6">
+        <div className="sticky z-20 top-0 bg-white max-w-8xl w-full sm:mx-auto py-5 px-6">
             <h3 className="font-bold text-indigo-700 uppercase">Introducing...🥁</h3>
             <p className="text-lg sm:text-2xl font-extrabold text-gray-800">Little Chuckle<span className="font-normal"> - Have a laugh</span> </p>
-            <hr className="my-1" />
+            <hr className="my-1.5" />
         </div>
     );
 }
